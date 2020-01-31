5a03764297c1a78aa1d5a96bfaec211998b43c82
hyunsojung-dev <thwjd2525@gmail.com>
2020-01-31 14:18:11 +0900

    mongo cloud + node server

--- certificate/server/index.js
+++ certificate/server/index.js
@@ -3,20 +3,24 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const config = require('../config');
 
-// DB setting
-mongoose.set('useNewUrlParser', true);    
-mongoose.set('useFindAndModify', false);  
-mongoose.set('useCreateIndex', true);     
-mongoose.connect(process.env.MongoDB); 
+mongoose.connect(config.db.uri, {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
 const db = mongoose.connection;
-db.once("open", function(){  console.log("DB connected");});
-db.on("error", function(err){  console.log("DB ERROR : ", err);});
 
-// Other settings
-//app.set("view engine", "ejs");
+const handleOpen = () => console.log("➡️ connected to DB");
+const handleError = (err) => console.log(`Error on DB Connection : ${err}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
+
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({extended:true})); 
+
 
 // 소스코드 수정 후 npm run-script build > node ./index.js 시작
 console.log(path.resolve(__dirname,'../build'));
@@ -24,7 +28,7 @@ app.use('/', express.static(path.resolve(__dirname,'../build')));
 
 //Router
 //라우터 설정 에러 문제 problem url 이후의 router 설정이기 때문에 /Problem/ 으로 나타내야함.
-const HomeRoute = express.Router();
+// const HomeRoute = express.Router();
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin","*");
@@ -33,16 +37,15 @@ app.use(function(req, res, next) {
 });
 
 app.use('/view/', require("../routes/routeHomeview")); // home 문제 랜덤 리스트 뿌려주는 라우터
-app.use("/Problem/", require("../routes/routeProblem"));
+app.use("/Problem/", require("../routes/routeProblem")); // 사용자들의 문제 요청 리스트 라우터
 app.use('/comment/', require("../routes/routeYear")); // 3번째 뷰의 년도별 문제 리스트 라우터
-app.use('/login/', require("../routes/routeLogin")); // Login 후 View 화면
-app.use('/home/', HomeRoute); // Login 후 View 화면
+// app.use('/login/', require("../routes/routeLogin")); // Login 후 View 화면
+// app.use('/home/', HomeRoute); // Login 후 View 화면
 
 //app.use('api/account/', require("../routes/account")); 
-
 
 // Port setting
 const port = 7376;
 app.listen(7376, function(){
-  console.log("server on! http://localhost:"+port);
+  console.log("➡️ server on! http://localhost:"+port);
 });
