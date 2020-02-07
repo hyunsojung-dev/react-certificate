bd7702a49d65e9eb3a0103cc56d31e758611b330
hyunsojung-dev <thwjd2525@gmail.com>
2020-02-07 15:59:37 +0900

    Updates

---
+++ precache-manifest.9e9c07c4577801daf0a28c9f81ea0f46.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88f397f2c0f14c7593b91d75ff936041",
+    "url": "/react-certificate/index.html"
+  },
+  {
+    "revision": "3dfec2cca8a8ddc652f8",
+    "url": "/react-certificate/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "88feb6ee2fadce3968be",
+    "url": "/react-certificate/static/css/main.264c0a4c.chunk.css"
+  },
+  {
+    "revision": "3dfec2cca8a8ddc652f8",
+    "url": "/react-certificate/static/js/2.908f1ca1.chunk.js"
+  },
+  {
+    "revision": "8f38180e792ac67b6ceee4752340cbc1",
+    "url": "/react-certificate/static/js/2.908f1ca1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "88feb6ee2fadce3968be",
+    "url": "/react-certificate/static/js/main.18ab5812.chunk.js"
+  },
+  {
+    "revision": "c8faaae1f92b94437e54",
+    "url": "/react-certificate/static/js/runtime-main.e6d4f844.js"
+  },
+  {
+    "revision": "30e91a6a053acf6be67cb8e150c7145f",
+    "url": "/react-certificate/static/media/iconlogin.30e91a6a.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c99e35fa87302bdf46706b1051cd7c60.js"
+  "/react-certificate/precache-manifest.9e9c07c4577801daf0a28c9f81ea0f46.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-certificate/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
